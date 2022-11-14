bf91b5c6b0a388ea54f1ae68685b1d9c17bf8aa7
unotovive <tec.unotovive@gmail.com>
2022-11-14 14:49:31 +0900

    feat: initial page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<style lang="scss" scoped>
+.page {
+  background: $bg_primary;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+<template>
+  <div class="page">top</div>
+</template>
+<script setup lang="ts"></script>
